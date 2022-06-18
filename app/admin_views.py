68747f665c52163e44f9ebc96f48ee9d73d7e03e
sechabro <96841300+sechabro@users.noreply.github.com>
2022-06-18 14:30:40 -0400

    2022.06.18 static/img folder add, matplotlib file make and dynamic serving tries

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,33 +1,74 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
-from flask import render_template, send_file
+from flask import render_template, send_file, Flask, send_from_directory, Response
 from app.models import InvoiceModel, ClientModel
 from sqlalchemy import Boolean, create_engine, insert, select, func, distinct, true, values 
 from sqlalchemy.orm import sessionmaker, declarative_base
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import savefig
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 import io
 import base64
-
+import random
+#Connect
 engine = create_engine('postgresql://postgres@localhost:5432/billit')
 Session = sessionmaker(bind=engine)
+session = Session()
+
+#Dashboard invoice data
+paid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == True).count()
+unpaid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == False).count()
+total_clients = session.query(ClientModel).count()
+total_inv = session.query(InvoiceModel).count()
+
+
 
 @app.route('/dashboard')
 def dashboard_index():
-    session = Session()
-    #Database Queries
-    paid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == True).count()
-    unpaid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == False).count()
-    total_clients = session.query(ClientModel).count()
-    return render_template('admin/dashboard.html', paid_inv = paid_inv, unpaid_inv = unpaid_inv, total_clients = total_clients)
 
-'''@app.route('/invoice_pie')
-def invoice_pie():
-    session = Session()
-    paid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == True).count()
-    unpaid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == False).count()
-    #Pie Graph
+    return render_template('admin/dashboard.html', paid_inv = paid_inv, unpaid_inv = unpaid_inv, total_inv=total_inv, total_clients = total_clients)
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(paid_inv)
+    ys1 = range(total_inv)
+    axis.hist(xs, ys1)
+    return fig
+    
+'''@app.route('/plot')
+def build_plot():
+    img = io.BytesIO()
     sections = [paid_inv, unpaid_inv]
     labels = ['Paid Invoices', 'Unpaid Invoices']
     colors = ['green', 'red']
-    pie = plt.pie(sections, labels=labels, wedgeprops={'edgecolor': 'black'}, colors=colors)
-    return send_file(pie, mimetype='img/png')'''
+    plt.title('Paid vs. Unpaid')
+    plt.pie(sections, labels=labels, wedgeprops={'edgecolor': 'black'}, colors=colors)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    return '<img src="data:image/png;base64,{}">'.format(plot_url)
+
+@app.route('/pie_graph.png')
+def invoice_pie():
+
+
+def create_figure():
+    sections = [paid_inv, unpaid_inv]
+    labels = ['Paid Invoices', 'Unpaid Invoices']
+    colors = ['green', 'red']
+    fig1, ax1 = plt.subplots()
+    ax1.title('Paid vs. Unpaid')
+    ax1.pie(sections, labels=labels, wedgeprops={'edgecolor': 'black'}, colors=colors)
+    ax1.axis('equal')
+    plt.savefig('/static/images/invoice_payment_data.png')
+    return send_from_directory(pie)'''
--- testing.py
+++ testing.py
@@ -1,14 +1,17 @@
 # %%
+from email.mime import base
 from flask_sqlalchemy import SQLAlchemy
 from app import app
-from flask import render_template, send_file
+from flask import render_template, send_file, Flask
 from app.models import InvoiceModel, ClientModel
 from sqlalchemy import Boolean, create_engine, insert, select, func, distinct, true, values 
 from sqlalchemy.orm import sessionmaker, declarative_base
 import numpy as np, pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigCan
+from matplotlib.backends.backend_template import FigureManagerTemplate
 import base64, io
+from matplotlib.figure import Figure
 engine = create_engine('postgresql://postgres@localhost:5432/billit')
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -20,11 +23,14 @@ unpaid_inv = session.query(InvoiceModel).filter(InvoiceModel.paid == False).coun
 
 # %%
 #Pie graphing total_inv
+fig = Figure()
 sections = [paid_inv, unpaid_inv]
 labels = ['Paid Invoices', 'Unpaid Invoices']
 colors = ['green', 'red']
 plt.title('Paid vs. Unpaid')
-pie = plt.pie(sections, labels=labels, wedgeprops={'edgecolor': 'black'}, colors=colors)
-pie
-#<img src="{{ url_for('invoice_pie') }}">
+plt.pie(sections, labels=labels, wedgeprops={'edgecolor': 'black'}, colors=colors)
+
+plt.show()
+
+
 # %%
